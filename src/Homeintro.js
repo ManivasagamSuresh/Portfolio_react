39de6bae07927067293428fe6e9d24f879d37228
ManivasagamSuresh <s.kishore123.64@gmail.com>
2023-01-09 18:26:28 +0530

    links worked,download balance

--- src/Homeintro.js
+++ src/Homeintro.js
@@ -6,16 +6,16 @@ import { BsLinkedin } from 'react-icons/bs';
 function Homeintro() {
   return (
     
-    <div className="boxintro " id="home"> 
-      <div className="row">
+    
+      <div className="row boxintro " id="home">
         <div className="col-lg-6 indexi1">
         <div id="home-intro">
         <div id="introname">Hi, I'm <span id="mani">Manivasagam</span></div> 
-        <div className="animate-character">FULL STACK DEVELOPER</div>
+        <div className="animate-character">FULL  STACK  DEVELOPER</div>
         <div className="homeicons" style={{marginTop: "20px",width:"20px"}} ><a href="https://github.com/ManivasagamSuresh" style={{color: "#fff"}} target="_blank"><AiOutlineGithub size={"3em"}/></a></div>
-        <div className="homeicons" style={{marginTop: "20px"}}><a href="https://www.linkedin.com/in/manivasagam-s-3534bb157/" style={{color: '0077b5'}} target="_blank"><BsLinkedin size={"3em"}/></a></div>
+        <div className="homeicons" style={{marginTop: "20px"}}><a href="https://www.linkedin.com/in/manivasagam-s-3534bb157/" style={{color: '#0077b5'}} target="_blank"><BsLinkedin size={"3em"}/></a></div>
         <br/>
-        <a href="Manivasagam resume.pdf" download className="btn btn-outline-warning" id="resumedownload" target="_blank">Download CV</a>  
+        <a href="resume.pdf" download className="btn btn-outline-warning" id="resumedownload" target="_blank">Download CV</a>  
       </div>
       </div>
       <div className="col-lg-6 indexi2" >
@@ -24,7 +24,7 @@ function Homeintro() {
         </div>
       </div>
       </div>
-    </div>
+    
   
 
 
--- src/Topbar.js
+++ src/Topbar.js
@@ -4,25 +4,23 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 function Topbar() {
   return (
-    <div className="row">
-    <div className="containernav col-lg-12">  
+    <div className="containernav">     
         <nav className="navbar navbar-expand-lg">
   <div className="container-fluid">
-    <button id="navbut"  onClick={()=>{console.log('hello')}}
+    <button id="navbut" 
  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-      <li className="nav-item"><a id="link1" href="#home" style={{textDecoration: "none"}} className="nav-link">Home</a></li> 
-        <li className="nav-item"><a id="link2" href="#about" style={{textDecoration: "none"}} className="nav-link">About</a></li>
-        <li className="nav-item"><a id="link4" href="#project" style={{textDecoration: "none"}} className="nav-link">Projects</a></li>
-        <li className="nav-item"><a id="link5" href="#contact" style={{textDecoration: "none"}} className="nav-link">Contact</a></li>        
+      <li className="nav-item"><a id="link1" href="#home" style={{textDecoration: "none",fontSize:"25px"}} className="nav-link">Home</a></li> 
+        <li className="nav-item"><a id="link2" href="#about" style={{textDecoration: "none",fontSize:"25px"}} className="nav-link">About</a></li>
+        <li className="nav-item"><a id="link4" href="#project" style={{textDecoration: "none",fontSize:"25px"}} className="nav-link">Projects</a></li>
+        <li className="nav-item"><a id="link5" href="#contact" style={{textDecoration: "none",fontSize:"25px"}} className="nav-link">Contact</a></li>        
       </div>
     </div>
   </div>
 </nav>
-    </div>
     </div>
 
   )
