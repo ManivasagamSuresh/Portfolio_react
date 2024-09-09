92fa00efff7db4e07b1f59da716ecb9a1060e7d4
Manivasagam Suresh <mani@ayoo.care>
2024-09-09 14:57:31 +0530

    contact and project page done

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -6,15 +6,17 @@ import Projectcard from '../Projectcard/Projectcard'
 
 function Project() {
   return (
-  <div className="row" id="project">
-    <div className="col-lg-12">
-      <h1 className="projects-heading display-5">PROJECTS</h1>
+  <div className="projectOuterContainer" id="project">
+    <div className="ProjectDescriptionContainer">
+      <h1 className="projects-heading ">PROJECTS</h1>
       <h2 className="projects-des">What I Built</h2>
+      <div className='credsContainer'>
       <h4>Credentials</h4>
       <div>Admin: admin@gmail.com, Password: admin</div>
-      <div style={{marginBottom:"50px"}}>User: test@gmail.com, Password: test </div>
+      <div >User: test@gmail.com, Password: test </div>
+      </div>
     </div>
-    <Projectcard/>
+    <Projectcard />
   </div>
   
 
--- src/Components/Projectcard/Projectcard.jsx
+++ src/Components/Projectcard/Projectcard.jsx
@@ -1,141 +1,229 @@
-import React from 'react'
-import { BiLinkExternal } from 'react-icons/bi';
-import { FiGithub } from 'react-icons/fi';
-import { VscGithub } from 'react-icons/vsc';
-import myntra from "../../images/myntra.png"
-import youtube from "../../images/youtube.png"
-import tournament from "../../images/tournament.png"
-import chat from "../../images/chat.png"
-import catalog from "../../images/item.png"
-
-
-
-
+import React from "react";
+import { BiLinkExternal } from "react-icons/bi";
+import { FiGithub } from "react-icons/fi";
+import { VscGithub } from "react-icons/vsc";
+import myntra from "../../images/myntra.png";
+import youtube from "../../images/youtube.png";
+import tournament from "../../images/tournament.png";
+import chat from "../../images/chat.png";
+import catalog from "../../images/item.png";
 
 function Projectcard() {
   return (
-    
-
-  <>
-    <div className="col-lg-12">
-      <div  className="card" >
-        
-        <div className="card-body">
-          <div className="card-d">
-          <h2 className="card-title">Fashion E-Commerce Website</h2>
-          <p className="card-text">Users can Sign-up, Sign-in with Email id. Secured with  JSON web token Authentication. Allows users to view, wishlist, move products to cart and buy the products with help of Razor pay payment gateway.</p>
-          <p><span className='card-tech'>Technologies and libraries used </span> :  React.js, Node.js, MongoDB, JWT, Redux Toolkit, Razor pay, Bootstrap</p>
-          <div className='projectcard-icons'>
-          <a className="card-link" href="https://grand-quokka-39f552.netlify.app" target="_blank"><BiLinkExternal size={"1.5em"} className="giticon"/>View Project</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/myntra-clone-frontend" target="_blank"><FiGithub size={"1.5em"} className="giticon"/>Front-End</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/myntra-clone-backend" target="_blank"><VscGithub size={"1.5em"} className="giticon"/>Back-End</a>
+    <div className="projectCardContainer">
+      <div className="card-body">
+        <div className="card-d">
+          <div className="card-title">Fashion E-Commerce Website (Mar 2023)</div>
+          <div className="card-text">
+            Users can Sign-up, Sign-in with Email id. Secured with JSON web token Authentication.
+            Allows users to view, wishlist, move products to cart and buy the products with help of
+            Razor pay payment gateway.
           </div>
+          <div>
+            <span className="card-tech">Technologies and libraries used: </span>{" "}
+            <span className="techUsed">
+              {" "}
+              React.js, Node.js, MongoDB, JWT, Redux Toolkit, Razor pay, Bootstrap
+            </span>
           </div>
-          <div className='card-i'>
+          <div className="projectcard-icons">
+            <a
+              className="card-link"
+              href="https://grand-quokka-39f552.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BiLinkExternal className="giticon" />
+              View Project
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/myntra-clone-frontend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub className="giticon" />
+              Front-End
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/myntra-clone-backend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <VscGithub className="giticon" />
+              Back-End
+            </a>
+          </div>
+        </div>
+        <div className="card-i">
           <img className="project-img" src={myntra} alt="" />
         </div>
-        </div>
-        
-      </div>
       </div>
 
-    
-    <div className="col-lg-12">
-      <div  className="card" >
-        
-        <div className="card-body">
-         <div className='card-d'>
-          <h2 className="card-title">Video Sharing - Social Media Platform</h2>
-          <p className="card-text">Users can Sign-up, Sign-in with Email id. Secured with  JSON web token Authentication. Allows users to view, like, dislike,  add comments to the video, subscribe a channel, and post a video.</p>
-          <p><span className='card-tech'>Technologies and libraries used </span> : React.js, Node.js, MongoDB,Tailwind CSS, JWT, Redux Toolkit, sockt.io, Firebase</p>
-          <div className='projectcard-icons'>
-          <a className="card-link" href="https://cheerful-valkyrie-754245.netlify.app" target="_blank"><BiLinkExternal size={"1.5em"} className="giticon"/>View Project</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/Youtube_frontend_tailwind" target="_blank"><FiGithub size={"1.5em"} className="giticon"/>Front-End</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/YoutubeClone-Backend" target="_blank"><VscGithub size={"1.5em"} className="giticon"/>Back-End</a>  
-          </div >
-         </div>
-         <div className='card-i'>
+      <div className="card-body">
+        <div className="card-d">
+          <h2 className="card-title">Video Sharing - Social Media Platform (Dec 2022)</h2>
+          <div className="card-text">
+            Users can Sign-up, Sign-in with Email id. Secured with JSON web token Authentication.
+            Allows users to view, like, dislike, add comments to the video, subscribe a channel, and
+            post a video.
+          </div>
+          <div>
+            <span className="card-tech">Technologies and libraries used: </span>{" "}
+            <span className="techUsed">
+              {" "}
+              React.js, Node.js, MongoDB,Tailwind CSS, JWT, Redux Toolkit, sockt.io, Firebase
+            </span>
+          </div>
+          <div className="projectcard-icons">
+            <a
+              className="card-link"
+              href="https://cheerful-valkyrie-754245.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BiLinkExternal size={"1.5em"} className="giticon" />
+              View Project
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/Youtube_frontend_tailwind"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub size={"1.5em"} className="giticon" />
+              Front-End
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/YoutubeClone-Backend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <VscGithub size={"1.5em"} className="giticon" />
+              Back-End
+            </a>
+          </div>
+        </div>
+        <div className="card-i">
           <img className="project-img" src={youtube} alt="" />
         </div>
-         
+      </div>
+
+      <div className="card-body">
+        <div className="card-d">
+          <h2 className="card-title">Tournament System App (Aug 2023)</h2>
+          <div className="card-text">
+            Users can Login and join their favorite tournament, cancel and edit their registration.
+            Admin can create, edit and delete the tournaments.
+          </div>
+          <div>
+            <span className="card-tech">Technologies and libraries used: </span>{" "}
+            <span className="techUsed">
+              {" "}
+              React.js, Node.js, MongoDB, TailwindCSS, Redux Toolkit{" "}
+            </span>
+          </div>
+          <div className="projectcard-icons">
+            <a
+              className="card-link"
+              href="https://steady-shortbread-751776.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BiLinkExternal size={"1.5em"} className="giticon" />
+              View Project
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/indiggFrontend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub size={"1.5em"} className="giticon" />
+              Front-End
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/indiggBackend"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <VscGithub size={"1.5em"} className="giticon" />
+              Back-End
+            </a>
+          </div>
         </div>
-      </div>
-      </div>
-
-
-
-      <div className="col-lg-12">
-      <div  className="card" >
-        
-        <div className="card-body">
-        <div className='card-d'>
-          <h2 className="card-title">Tournament System App</h2>
-          <p className="card-text">Users can Login and join their favorite tournament, cancel and edit their registration. Admin can create, edit and delete the tournaments.</p>
-          <p><span className='card-tech'>Technologies and libraries used </span> : React.js, Node.js, MongoDB, TailwindCSS, Redux Toolkit</p>
-          <div className='projectcard-icons'>
-          <a className="card-link" href="https://steady-shortbread-751776.netlify.app/" target="_blank"><BiLinkExternal size={"1.5em"} className="giticon"/>View Project</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/indiggFrontend" target="_blank"><FiGithub size={"1.5em"}className="giticon"/>Front-End</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/indiggBackend" target="_blank"><VscGithub size={"1.5em"} className="giticon"/>Back-End</a>
-          </div>
-          </div>
-          <div className='card-i'>
+        <div className="card-i">
           <img className="project-img" src={tournament} alt="" />
         </div>
+      </div>
+
+      <div className="card-body">
+        <div className="card-d">
+          <h2 className="card-title">Chat-App (Feb 2023)</h2>
+          <div className="card-text">
+            Users can Login with their G-mail. Create a chat box ,receive and send messages in the
+            chat box
+          </div>
+          <div>
+            <span className="card-tech">Technologies and libraries used: </span>{" "}
+            <span className="techUsed">React.js, Node.js, Mongoose, Pusher, Firebase </span>
+          </div>
+          <div className="projectcard-icons">
+            <a
+              className="card-link"
+              href="https://capable-gelato-a223ec.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BiLinkExternal size={"1.5em"} className="giticon" />
+              View Project
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/ChatApp-FrontEnd"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FiGithub size={"1.5em"} className="giticon" />
+              Front-End
+            </a>
+            <a
+              className="card-link"
+              href="https://github.com/ManivasagamSuresh/ChatApp-backend-"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <VscGithub size={"1.5em"} className="giticon" />
+              Back-End
+            </a>
+          </div>
         </div>
-      </div>
-      </div>
-
-
-      <div className="col-lg-12">
-      <div  className="card" >
-        
-        <div className="card-body">
-        <div className='card-d'>
-          <h2 className="card-title">Chat-App</h2>
-          <p className="card-text">Users can Login with their G-mail. Create a chat box ,receive and send messages in the chat box</p>
-          <p><span className='card-tech'>Technologies and libraries used </span> : React.js, Node.js, Mongoose, Pusher, Firebase </p>
-          <div className='projectcard-icons'>
-          <a className="card-link" href="https://capable-gelato-a223ec.netlify.app" target="_blank"><BiLinkExternal size={"1.5em"} className="giticon"/>View Project</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/ChatApp-FrontEnd" target="_blank"><FiGithub size={"1.5em"}className="giticon"/>Front-End</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/ChatApp-backend-" target="_blank"><VscGithub size={"1.5em"} className="giticon"/>Back-End</a>
-          </div>
-          </div>
-          <div className='card-i'>
+        <div className="card-i">
           <img className="project-img" src={chat} alt="" />
         </div>
-        </div>
-      </div>
       </div>
 
-
-      <div className="col-lg-12">
-      <div  className="card" >
-        
+      {/* 
         <div className="card-body">
         <div className='card-d'>
           <h2 className="card-title">Item Catalog</h2>
-          <p className="card-text">Users can view the Products and its details.Admin can create,edit and delete the products.</p>
-          <p><span className='card-tech'>Technologies and libraries used </span> : React.js, Node.js, MongoDB, Bootstrap  </p>
+          <div className="card-text">Users can view the Products and its details.Admin can create,edit and delete the products.</div>
+          <div><span className='card-tech'>Technologies and libraries used </span> : React.js, Node.js, MongoDB, Bootstrap  </div>
           <div className='projectcard-icons'>
-          <a className="card-link" href="https://preeminent-frangipane-88b7b3.netlify.app" target="_blank"><BiLinkExternal size={"1.5em"} className="giticon"/>View Project</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/capstone-frontend" target="_blank"><FiGithub size={"1.5em"}className="giticon"/>Front-End</a>
-          <a className="card-link" href="https://github.com/ManivasagamSuresh/capstone-backend" target="_blank"><VscGithub size={"1.5em"} className="giticon"/>Back-End</a>
+          <a className="card-link" href="https://preeminent-frangipane-88b7b3.netlify.app" target="_blank"  rel="noreferrer"><BiLinkExternal size={"1.5em"} className="giticon"/>View Project</a>
+          <a className="card-link" href="https://github.com/ManivasagamSuresh/capstone-frontend" target="_blank"  rel="noreferrer"><FiGithub size={"1.5em"}className="giticon"/>Front-End</a>
+          <a className="card-link" href="https://github.com/ManivasagamSuresh/capstone-backend" target="_blank"  rel="noreferrer"><VscGithub size={"1.5em"} className="giticon"/>Back-End</a>
           </div>
           </div>
           <div className='card-i'>
           <img className="project-img" src={catalog} alt="" />
         </div>
-        </div>
-      </div>
-      </div>
-
-
-
-
-  </>
-  
-
-  )
+        </div> */}
+    </div>
+  );
 }
 
-export default Projectcard
+export default Projectcard;
